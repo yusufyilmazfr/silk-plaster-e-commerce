9b56625ce76594290d096b6976cc1efaab5b5940
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-15 02:10:21 +0300

    database tables added to the context

---
+++ SilkPlaster/SilkPlaster.DataAccessLayer/EntityFramework/DatabaseContext.cs
@@ -0,0 +1,34 @@
+﻿using SilkPlaster.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.DataAccessLayer.EntityFramework
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DatabaseContext")
+        {
+            Database.SetInitializer(new MyInitializer());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<WishList> WishLists { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
