565e08530a59fa7f5b73dc33dfa39aca88ce1ff3
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-15 01:54:41 +0300

    base class filled

--- SilkPlaster/SilkPlaster.Entities/Abstract/EntityBase.cs
+++ SilkPlaster/SilkPlaster.Entities/Abstract/EntityBase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace SilkPlaster.Entities.Abstract
 {
     public class EntityBase
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, Column(TypeName = "datetime2")]
+        public DateTime AddedDate { get; set; }
+        [Column(TypeName = "datetime2")]
+        public DateTime ModifiedDate { get; set; }
     }
 }
