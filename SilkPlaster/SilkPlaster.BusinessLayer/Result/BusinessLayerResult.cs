612fdfddc3f0ffb77a08d303a6a1e8522a494a36
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-16 22:21:51 +0300

    states and errors returned to the UI layer are classed

---
+++ SilkPlaster/SilkPlaster.BusinessLayer/Result/BusinessLayerResult.cs
@@ -0,0 +1,25 @@
+﻿using SilkPlaster.Common.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.BusinessLayer.Result
+{
+    public class BusinessLayerResult<T>
+    {
+        public BusinessLayerResult()
+        {
+            Errors = new List<ErrorMessageObject>();
+        }
+
+        public List<ErrorMessageObject> Errors { get; set; }
+        public T Result { get; set; }
+
+        public void AddError(ErrorMessageCode messageCode, string message)
+        {
+            Errors.Add(new ErrorMessageObject { ErrorCode = messageCode, ErrorMessage = message });
+        }
+    }
+}
