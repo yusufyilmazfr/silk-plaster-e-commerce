35c6a0d7060d693f5b367ebc799e06a643d5848c
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-16 23:10:02 +0300

    some manager classes added

---
+++ SilkPlaster/SilkPlaster.BusinessLayer/OrderManager.cs
@@ -0,0 +1,15 @@
+﻿using SilkPlaster.BusinessLayer.Abstract;
+using SilkPlaster.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.BusinessLayer
+{
+    public class OrderManager : ManagerBase<Order>
+    {
+
+    }
+}
