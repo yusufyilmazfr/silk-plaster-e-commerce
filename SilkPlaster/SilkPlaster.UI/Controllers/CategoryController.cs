023929c59768a053517390df6d0c3c474ff89d30
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-16 23:07:19 +0300

    this controller created for list fetching

---
+++ SilkPlaster/SilkPlaster.UI/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using SilkPlaster.BusinessLayer;
+using SilkPlaster.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilkPlaster.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager _categoryManager = new CategoryManager();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult GetCategories()
+        {
+            List<CategoryModel> categories = _categoryManager
+                .ListQueryable()
+                .Include("Products")
+                .Select(i => new CategoryModel
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    ProductCount = i.Products.Count
+                }).ToList();
+
+            return PartialView(categories);
+        }
+    }
+}
