b3b684ce642fb1ef8f4309cdf3f8fda4f03d4c5f
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-16 23:10:38 +0300

    added manager class for commenting controls

---
+++ SilkPlaster/SilkPlaster.BusinessLayer/CommentManager.cs
@@ -0,0 +1,29 @@
+﻿using SilkPlaster.BusinessLayer.Abstract;
+using SilkPlaster.BusinessLayer.Result;
+using SilkPlaster.Common.Message;
+using SilkPlaster.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.BusinessLayer
+{
+    public class CommentManager : ManagerBase<Comment>
+    {
+        public new BusinessLayerResult<Comment> Insert(Comment obj)
+        {
+            BusinessLayerResult<Comment> layerResult = new BusinessLayerResult<Comment>();
+
+            int count = base.Insert(obj);
+
+            if (count == 0)
+            {
+                layerResult.AddError(ErrorMessageCode.FailedToAddRecord, "Kayıt eklenemedi!");
+            }
+
+            return layerResult;
+        }
+    }
+}
