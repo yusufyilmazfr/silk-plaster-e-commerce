58a7d9bdece4ffdb27c2f18250baa98dce3c02d5
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-16 23:12:41 +0300

    basket operations done

---
+++ SilkPlaster/SilkPlaster.UI/Controllers/BasketController.cs
@@ -0,0 +1,71 @@
+﻿using SilkPlaster.BusinessLayer;
+using SilkPlaster.UI.Models;
+using SilkPlaster.UI.Models.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilkPlaster.UI.Controllers
+{
+    public class BasketController : Controller
+    {
+        WishListManager _wishListManager = new WishListManager();
+        BasketManager _basketManager = new BasketManager();
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult WiewQuicklyBasket()
+        {
+            int loggedInMemberId = CurrentSession.Member.Id;
+
+            List<BasketModel> baskets = _basketManager
+                .ListQueryable()
+                .Include("Product")
+                .Include("Member")
+                .Where(i => i.Member.Id == loggedInMemberId)
+                .Select(j => new BasketModel
+                {
+                    Id = j.Id,
+                    Quantity = j.ProductCount,
+                    Product = new ProductDetailsModel
+                    {
+                        Id = j.Product.Id,
+                        Name = j.Product.Name,
+                        NewPrice = j.Product.NewPrice,
+                        FirstImage = j.Product.FirstImage
+                    }
+                })
+                .ToList();
+
+
+            return PartialView(baskets);
+        }
+
+        public PartialViewResult WiewQuicklyWishList()
+        {
+            int loggedInMemberId = CurrentSession.Member.Id;
+
+            List<ProductDetailsModel> products = _wishListManager
+                .ListQueryable()
+                .Include("Product")
+                .Include("Member")
+                .Where(i => i.Member.Id == loggedInMemberId)
+                .Select(i => new ProductDetailsModel
+                {
+                    Id = i.Product.Id,
+                    Name = i.Product.Name,
+                    FirstImage = i.Product.FirstImage,
+                    NewPrice = i.Product.NewPrice
+                }).ToList();
+
+            return PartialView(products);
+        }
+    }
+}
