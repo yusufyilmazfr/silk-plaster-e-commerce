6050f8038eaff7ccf72630b4858411cb3cae2f9f
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-16 22:20:11 +0300

    ManagerBase class created to automatically use functions in the UI layer

---
+++ SilkPlaster/SilkPlaster.BusinessLayer/Abstract/ManagerBase.cs
@@ -0,0 +1,58 @@
+﻿using SilkPlaster.Core.Abstract;
+using SilkPlaster.DataAccessLayer.EntityFramework;
+using SilkPlaster.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.BusinessLayer.Abstract
+{
+    public abstract class ManagerBase<T> : IRepository<T> where T : class, IEntity, new()
+    {
+        Repository<T> _repository = new Repository<T>();
+
+        public virtual int Delete(T obj)
+        {
+            return _repository.Delete(obj);
+        }
+
+        public virtual T Find(Expression<Func<T, bool>> where)
+        {
+            return _repository.Find(where);
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public virtual List<T> GetAll(Expression<Func<T, bool>> where)
+        {
+            return _repository.GetAll(where);
+        }
+
+        public virtual int Insert(T obj)
+        {
+            return _repository.Insert(obj);
+        }
+
+        public virtual IQueryable<T> ListQueryable()
+        {
+            return _repository.ListQueryable();
+        }
+
+        public virtual int Save()
+        {
+            return _repository.Save();
+        }
+
+        public virtual int Update(T obj)
+        {
+            return _repository.Update(obj);
+        }
+
+    }
+}
