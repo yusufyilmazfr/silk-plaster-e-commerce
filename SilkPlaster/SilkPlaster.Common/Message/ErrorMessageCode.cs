22604e727b2bcdad91155bfb0fbfa41064be24e3
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-16 22:33:46 +0300

    error messages and object generated for returning BusinessLayerResult object

---
+++ SilkPlaster/SilkPlaster.Common/Message/ErrorMessageCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.Common.Message
+{
+    public enum ErrorMessageCode
+    {
+        MemberAlreadyExists = 100,
+        EmailAlreadyExists = 101,
+        FailedToAddRecord = 102,
+        ObjectAlreadyExists = 103,
+        ObjectNotFound = 104,
+        FailedToDeleteRecord = 105
+    }
+}
---
+++ SilkPlaster/SilkPlaster.Common/Message/ErrorMessageObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.Common.Message
+{
+    public class ErrorMessageObject
+    {
+        public ErrorMessageCode ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
