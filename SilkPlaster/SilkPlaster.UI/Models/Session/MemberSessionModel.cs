47c603b7d2b51e8bbe67fd616125276b4e9f7c2a
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-18 23:15:04 +0300

    edited

--- SilkPlaster/SilkPlaster.UI/Models/Session/MemberSessionModel.cs
+++ SilkPlaster/SilkPlaster.UI/Models/Session/MemberSessionModel.cs
@@ -1,15 +1,13 @@
-﻿using System;
+﻿using SilkPlaster.UI.Models.Session.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace SilkPlaster.UI.Models.Session
 {
-    public class MemberSessionModel
+    public class MemberSessionModel : PersonSessionModel
     {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+
     }
 }
