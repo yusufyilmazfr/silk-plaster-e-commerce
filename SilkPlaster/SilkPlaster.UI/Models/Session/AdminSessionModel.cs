5f8887f3ef5b54d5aecfa0b891ee5db2a9b04162
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-18 23:14:54 +0300

    this session model created for admin

---
+++ SilkPlaster/SilkPlaster.UI/Models/Session/AdminSessionModel.cs
@@ -0,0 +1,13 @@
+﻿using SilkPlaster.UI.Models.Session.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SilkPlaster.UI.Models.Session
+{
+    public class AdminSessionModel : PersonSessionModel
+    {
+
+    }
+}
