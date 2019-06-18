289f7256cc1f020c3d9fca86d0349efa7381c6f7
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-18 23:14:15 +0300

    refactoring: base PersonSessionModel created and MemberSessionModel inherited this class

---
+++ SilkPlaster/SilkPlaster.UI/Models/Session/Abstract/PersonSessionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SilkPlaster.UI.Models.Session.Abstract
+{
+    public class PersonSessionModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
