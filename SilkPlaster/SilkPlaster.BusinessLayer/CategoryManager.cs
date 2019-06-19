9068da394ab5e7505b3b4973c3ddc0e13a5eb5f2
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-19 15:15:08 +0300

    some manager classes edited

--- SilkPlaster/SilkPlaster.BusinessLayer/CategoryManager.cs
+++ SilkPlaster/SilkPlaster.BusinessLayer/CategoryManager.cs
@@ -1,4 +1,6 @@
 ﻿using SilkPlaster.BusinessLayer.Abstract;
+using SilkPlaster.BusinessLayer.Result;
+using SilkPlaster.Common.Message;
 using SilkPlaster.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,39 @@ namespace SilkPlaster.BusinessLayer
 {
     public class CategoryManager : ManagerBase<Category>
     {
+        BusinessLayerResult<Category> layerResult = new BusinessLayerResult<Category>();
 
+        public new BusinessLayerResult<Category> Update(Category obj)
+        {
+            Category category = base.Find(i => i.Id == obj.Id);
+
+            if (category == null)
+            {
+                layerResult.AddError(ErrorMessageCode.ObjectNotFound, "Böyle bir kategori bulunmamaktadır!");
+                return layerResult;
+            }
+
+            category.Name = obj.Name;
+            category.Description = obj.Description;
+
+            int count = base.Update(category);
+
+            if (count == 0)
+            {
+                layerResult.AddError(ErrorMessageCode.FailedToModifiedRecord, "Kategori düzenlenemedi!");
+            }
+            return layerResult;
+        }
+
+        public new BusinessLayerResult<Category> Insert(Category obj)
+        {
+            int count = base.Insert(obj);
+
+            if (count > 0)
+            {
+                layerResult.Result = obj;
+            }
+            return layerResult;
+        }
     }
 }
--- SilkPlaster/SilkPlaster.BusinessLayer/MemberManager.cs
+++ SilkPlaster/SilkPlaster.BusinessLayer/MemberManager.cs
@@ -13,10 +13,10 @@ namespace SilkPlaster.BusinessLayer
 {
     public class MemberManager : ManagerBase<Member>
     {
-        public BusinessLayerResult<Member> Register(RegisterViewModel model)
+        public BusinessLayerResult<Member> Register(RegisterViewModel obj)
         {
             BusinessLayerResult<Member> layerResult = new BusinessLayerResult<Member>();
-            layerResult.Result = Find(i => i.Email == model.Email);
+            layerResult.Result = Find(i => i.Email == obj.Email);
 
             if (layerResult.Result != null)
             {
@@ -25,18 +25,57 @@ namespace SilkPlaster.BusinessLayer
                 return layerResult;
             }
 
-            int insertCount = Insert(new Member
+            int insertCount = base.Insert(new Member
             {
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                Email = model.Email,
-                Password = model.Password
+                FirstName = obj.FirstName,
+                LastName = obj.LastName,
+                Email = obj.Email,
+                Password = obj.Password
             });
 
             if (insertCount > 0)
             {
-                layerResult.Result = Find(i => i.Email == model.Email && i.Password == model.Password);
+                layerResult.Result = Find(i => i.Email == obj.Email && i.Password == obj.Password);
             }
+            return layerResult;
+        }
+
+        public new BusinessLayerResult<Member> Update(Member obj)
+        {
+            BusinessLayerResult<Member> layerResult = new BusinessLayerResult<Member>();
+            Member member = base.Find(i => i.Id != obj.Id && i.Email == obj.Email);
+
+            layerResult.Result = obj;
+
+            if (member != null)
+            {
+                if (member.Email == obj.Email)
+                {
+                    layerResult.AddError(ErrorMessageCode.EmailAlreadyExists, "Böyle bir E-posta zaten kayıtlı!");
+                }
+                return layerResult;
+            }
+
+            layerResult.Result = base.Find(i => i.Id == obj.Id);
+
+            if (layerResult.Result == null)
+            {
+                layerResult.AddError(ErrorMessageCode.UserNotFound, "Böyle bir kullanıcı bulunmamaktadır!");
+                return layerResult;
+            }
+
+            layerResult.Result.FirstName = obj.FirstName;
+            layerResult.Result.LastName = obj.LastName;
+            layerResult.Result.Email = obj.Email;
+            layerResult.Result.Password = obj.Password;
+
+            int count = base.Update(layerResult.Result);
+
+            if (count == 0)
+            {
+                layerResult.AddError(ErrorMessageCode.FailedToModifiedRecord, "Kullanıcı düzenlenemedi!");
+            }
+
             return layerResult;
         }
     }
