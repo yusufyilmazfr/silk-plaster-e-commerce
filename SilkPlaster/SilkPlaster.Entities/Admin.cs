b35e50675b395425b51ff0efaff9682f9b9b0f72
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-19 15:08:11 +0300

    admin table added in context

---
+++ SilkPlaster/SilkPlaster.Entities/Admin.cs
@@ -0,0 +1,36 @@
+﻿using SilkPlaster.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.Entities
+{
+    [Table("Admins")]
+    public class Admin : EntityBase, IEntity
+    {
+        [Required,
+        DisplayName("İsim"),
+        StringLength(25, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string FirstName { get; set; }
+
+        [Required,
+        DisplayName("Soyisim"),
+        StringLength(25, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string LastName { get; set; }
+
+        [Required,
+        DisplayName("E-posta"),
+        StringLength(70, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string Email { get; set; }
+
+        [Required,
+        DisplayName("Parola"),
+        StringLength(64, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string Password { get; set; }
+    }
+}
