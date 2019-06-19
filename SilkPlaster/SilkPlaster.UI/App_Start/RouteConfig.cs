e04a9938cc48f17c94b6fab9820979e7190ef840
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-19 15:19:36 +0300

    main route config namesace edited to avoid confusion due to admin area usage

--- SilkPlaster/SilkPlaster.UI/App_Start/RouteConfig.cs
+++ SilkPlaster/SilkPlaster.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace SilkPlaster.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "SilkPlaster.UI.Controllers" }
             );
         }
     }
