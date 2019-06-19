c1070eecd0217096a2adad5adefb32d6bd5ca0be
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-19 15:09:02 +0300

    image helper class created

---
+++ SilkPlaster/SilkPlaster.UI/Models/Helpers/ImageHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SilkPlaster.UI.Models.Helpers
+{
+    public static class ImageHelper
+    {
+
+        public static bool IsTypeImage(string mimeType)
+        {
+            string[] mimeTypes = { "image/jpeg", "image/png", "image/gif" };
+
+            return mimeTypes.Contains(mimeType);
+        }
+
+        public static string CreateUniqueString()
+        {
+            return String.Format("{0:yyyyMMddHHmmss}", DateTime.Now);
+        }
+    }
+}
