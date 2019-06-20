b58606f8c725624f076b3bafae3e48abd32f069e
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-20 20:21:52 +0300

    slider controller created

---
+++ SilkPlaster/SilkPlaster.UI/Controllers/SliderController.cs
@@ -0,0 +1,19 @@
+﻿using SilkPlaster.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilkPlaster.UI.Controllers
+{
+    public class SliderController : Controller
+    {
+        SliderManager _sliderManager = new SliderManager();
+
+        public PartialViewResult Index()
+        {
+            return PartialView(_sliderManager.GetAll());
+        }
+    }
+}
