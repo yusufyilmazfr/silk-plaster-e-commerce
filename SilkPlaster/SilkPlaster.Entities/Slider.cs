f7e40c0e9994cbb4f6d28d6be2a4045437a7a6b7
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-20 20:20:38 +0300

    slider entities created

---
+++ SilkPlaster/SilkPlaster.Entities/Slider.cs
@@ -0,0 +1,32 @@
+﻿using SilkPlaster.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.Entities
+{
+    public class Slider : EntityBase, IEntity
+    {
+        [Required,
+        DisplayName("Ad"),
+        MaxLength(50, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string Name { get; set; }
+
+        [Required,
+        DisplayName("Açıklama"),
+        MaxLength(50, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string Description { get; set; }
+
+        [Required,
+        DisplayName("Fotoğraf")]
+        public string Image { get; set; }
+
+        [Required,
+        DisplayName("Yönleneceği Adres")]
+        public string RedirectAddress { get; set; }
+    }
+}
