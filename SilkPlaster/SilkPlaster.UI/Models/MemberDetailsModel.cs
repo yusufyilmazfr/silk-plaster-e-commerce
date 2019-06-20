607d8b3db2994936d589e6199c2dc593cd4d4b84
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-20 20:23:45 +0300

    some changes made

--- SilkPlaster/SilkPlaster.UI/Models/MemberDetailsModel.cs
+++ SilkPlaster/SilkPlaster.UI/Models/MemberDetailsModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +10,22 @@ namespace SilkPlaster.UI.Models
     public class MemberDetailsModel
     {
         public int Id { get; set; }
+
+        [Required,
+        DisplayName("İsim"),
+        StringLength(25, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
         public string FirstName { get; set; }
+
+        [Required,
+        DisplayName("Soyisim"),
+        StringLength(25, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
         public string LastName { get; set; }
+
+        [Required,
+        DisplayName("E-posta"),
+        DataType(DataType.EmailAddress, ErrorMessage = "{0} alanı E-posta formatında olmalıdır"),
+        StringLength(70, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
         public string Email { get; set; }
-        public DateTime AddedDate { get; set; }
+
     }
 }
---
+++ SilkPlaster/SilkPlaster.UI/Models/MemberPasswordModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SilkPlaster.UI.Models
+{
+    public class MemberPasswordModel
+    {
+        [Required,
+        DisplayName("Şu anki parola"),
+        StringLength(64, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string Password { get; set; }
+
+        [Required,
+        DisplayName("Yeni parola"),
+        StringLength(64, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string NewPassword { get; set; }
+
+        [Required,
+        DisplayName("Yeni parola tekrar"),
+        Compare("NewPassword", ErrorMessage = "Yeni girdiğiniz parolalar eşleşmiyor."),
+        StringLength(64, ErrorMessage = "{0} alanı max. {1} karakter olmalıdır.")]
+        public string NewRePassword { get; set; }
+    }
+}
