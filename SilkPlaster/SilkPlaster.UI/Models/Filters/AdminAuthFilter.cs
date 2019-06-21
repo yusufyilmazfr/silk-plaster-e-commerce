a9b09f6bf31b4546c222e21e41ab62910aa4ee1a
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-21 04:19:19 +0300

    I wrote a filter to see if the admin is logged in

---
+++ SilkPlaster/SilkPlaster.UI/Models/Filters/AdminAuthFilter.cs
@@ -0,0 +1,26 @@
+﻿using SilkPlaster.UI.Models.Helpers;
+using SilkPlaster.UI.Models.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilkPlaster.UI.Models.Filters
+{
+    public class AdminAuthFilter : FilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (CurrentSession.Get<AdminSessionModel>("Admin") == null)
+            {
+                string returnUrl = filterContext.HttpContext.Request.Url.AbsoluteUri;
+
+                filterContext.Result = new RedirectToRouteResult
+                    (
+                    new System.Web.Routing.RouteValueDictionary(new { controller = "Home", action = "Index", returnUrl = returnUrl })
+                    );
+            }
+        }
+    }
+}
