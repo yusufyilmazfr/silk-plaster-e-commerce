26a6f81446caf3bce8cb84c71d979f0dc8235bca
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-21 04:17:42 +0300

    I wrote a filter to see if the member is logged in

---
+++ SilkPlaster/SilkPlaster.UI/Models/Filters/MemberAuthFilter.cs
@@ -0,0 +1,25 @@
+﻿using SilkPlaster.UI.Models.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilkPlaster.UI.Models.Filters
+{
+    public class MemberAuthFilter : FilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (!CurrentSession.MemberIsLogged)
+            {
+                string returnUrl = filterContext.HttpContext.Request.Url.AbsoluteUri;
+
+                filterContext.Result = new RedirectToRouteResult
+                    (
+                        new System.Web.Routing.RouteValueDictionary(new { controller = "Account", action = "Login", returnUrl = returnUrl })
+                    );
+            }
+        }
+    }
+}
