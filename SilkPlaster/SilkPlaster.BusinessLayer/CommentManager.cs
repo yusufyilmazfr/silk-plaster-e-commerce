090f753700719feb22bfa08aea343182c0b2c198
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-23 02:08:12 +0300

    manager class modified for comment operations

--- SilkPlaster/SilkPlaster.BusinessLayer/CommentManager.cs
+++ SilkPlaster/SilkPlaster.BusinessLayer/CommentManager.cs
@@ -12,10 +12,10 @@ namespace SilkPlaster.BusinessLayer
 {
     public class CommentManager : ManagerBase<Comment>
     {
+        BusinessLayerResult<Comment> layerResult = new BusinessLayerResult<Comment>();
+
         public new BusinessLayerResult<Comment> Insert(Comment obj)
         {
-            BusinessLayerResult<Comment> layerResult = new BusinessLayerResult<Comment>();
-
             int count = base.Insert(obj);
 
             if (count == 0)
@@ -23,6 +23,29 @@ namespace SilkPlaster.BusinessLayer
                 layerResult.AddError(ErrorMessageCode.FailedToAddRecord, "Kayıt eklenemedi!");
             }
 
+            return layerResult;
+        }
+
+        public new BusinessLayerResult<Comment> Update(Comment obj)
+        {
+            layerResult.Result = base.Find(i => i.Id == obj.Id);
+
+            if (layerResult.Result == null)
+            {
+                layerResult.AddError(ErrorMessageCode.ObjectNotFound, "Böyle bir yorum bulunmamaktadır!");
+                return layerResult;
+            }
+
+            layerResult.Result.Text = obj.Text;
+            layerResult.Result.IsValid = obj.IsValid;
+            layerResult.Result.StarCount = obj.StarCount;
+
+            int count = base.Update(layerResult.Result);
+
+            if (count == 0)
+            {
+                layerResult.AddError(ErrorMessageCode.FailedToModifiedRecord, "Yorum güncellenemedi!");
+            }
             return layerResult;
         }
     }
