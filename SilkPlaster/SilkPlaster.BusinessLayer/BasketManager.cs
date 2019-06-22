855033edafc64f710712155e2c9d72832a54a7fa
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-22 20:42:32 +0300

    this manager class created for basket operations

--- SilkPlaster/SilkPlaster.BusinessLayer/BasketManager.cs
+++ SilkPlaster/SilkPlaster.BusinessLayer/BasketManager.cs
@@ -1,4 +1,6 @@
 ﻿using SilkPlaster.BusinessLayer.Abstract;
+using SilkPlaster.BusinessLayer.Result;
+using SilkPlaster.Common.Message;
 using SilkPlaster.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,25 @@ namespace SilkPlaster.BusinessLayer
 {
     public class BasketManager : ManagerBase<Basket>
     {
+        public new BusinessLayerResult<Basket> Delete(Basket obj)
+        {
+            BusinessLayerResult<Basket> layerResult = new BusinessLayerResult<Basket>();
 
+            layerResult.Result = base.Find(i => i.MemberId == obj.MemberId && i.ProductId == obj.ProductId);
+
+            if (layerResult.Result == null)
+            {
+                layerResult.AddError(ErrorMessageCode.ObjectNotFound, "Böyle bir ürün bulunmamaktadır!");
+                return layerResult;
+            }
+
+            int count = base.Delete(layerResult.Result);
+
+            if (count == 0)
+            {
+                layerResult.AddError(ErrorMessageCode.FailedToDeleteRecord, "Ürün silinemedi!");
+            }
+            return layerResult;
+        }
     }
 }
