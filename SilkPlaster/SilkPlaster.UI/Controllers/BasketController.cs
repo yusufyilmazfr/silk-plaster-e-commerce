847cf77ceab6798b5cf67f140f830ff7bd947b37
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-22 20:43:41 +0300

    products in the basket are listed

--- SilkPlaster/SilkPlaster.UI/Controllers/BasketController.cs
+++ SilkPlaster/SilkPlaster.UI/Controllers/BasketController.cs
@@ -1,4 +1,6 @@
 ﻿using SilkPlaster.BusinessLayer;
+using SilkPlaster.BusinessLayer.Result;
+using SilkPlaster.Entities;
 using SilkPlaster.UI.Models;
 using SilkPlaster.UI.Models.Filters;
 using SilkPlaster.UI.Models.Helpers;
@@ -16,9 +18,53 @@ namespace SilkPlaster.UI.Controllers
         WishListManager _wishListManager = new WishListManager();
         BasketManager _basketManager = new BasketManager();
 
+        [MemberAuthFilter]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [MemberAuthFilter]
+        public PartialViewResult MyBasket()
+        {
+            int loggedInMemberId = CurrentSession.Member.Id;
+
+            List<BasketModel> baskets = _basketManager
+                .ListQueryable()
+                .Include("Product")
+                .Include("Member")
+                .Where(i => i.MemberId == loggedInMemberId)
+                .Select(i => new BasketModel
+                {
+                    Id = i.Id,
+                    Quantity = i.ProductCount,
+                    Product = new ProductDetailsModel
+                    {
+                        Id = i.Product.Id,
+                        Name = i.Product.Name,
+                        NewPrice = i.Product.NewPrice,
+                        FirstImage = i.Product.FirstImage
+                    }
+                }).ToList()
+
+                .ToList();
+
+            return PartialView(baskets);
+        }
+
+        [MemberAuthFilter]
+        public PartialViewResult GetBasketItemCount()
+        {
+            int loggedInMemberId = CurrentSession.Member.Id;
+
+            int count = _basketManager
+                .ListQueryable()
+                .Include("Product")
+                .Include("Member")
+                .Where(i => i.Member.Id == loggedInMemberId)
+                .Count();
+
+            return PartialView(count);
         }
 
         [MemberAuthFilter]
@@ -69,5 +115,30 @@ namespace SilkPlaster.UI.Controllers
 
             return PartialView(products);
         }
+
+        [MemberAuthFilter]
+        public JsonResult Remove(int productId)
+        {
+            if (ModelState.IsValid)
+            {
+                int loggedInMemberId = CurrentSession.Member.Id;
+
+                BusinessLayerResult<Basket> layerResult = _basketManager.Delete(new Basket
+                {
+                    ProductId = productId,
+                    MemberId = loggedInMemberId
+                });
+
+                if (layerResult.Errors.Count == 0)
+                {
+                    return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+                }
+                return Json(new { result = false, message = layerResult.Errors.FirstOrDefault() }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false, message = "lütfen bütün alanları doldurunuz!" }, JsonRequestBehavior.AllowGet);
+
+        }
+
     }
 }
