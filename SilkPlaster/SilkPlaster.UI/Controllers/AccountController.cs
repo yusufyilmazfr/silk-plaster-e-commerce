c0b4d414e100845928fc1c86ac7a3dbe087e49ff
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-27 23:14:03 +0300

    CRUD operations done

--- SilkPlaster/SilkPlaster.UI/Controllers/AccountController.cs
+++ SilkPlaster/SilkPlaster.UI/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using SilkPlaster.UI.Models.Session;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -191,7 +192,163 @@ namespace SilkPlaster.UI.Controllers
         [MemberAuthFilter]
         public ActionResult MyAddresses()
         {
+            int loggedInMemberId = CurrentSession.Member.Id;
+
+            AddressManager addressManager = new AddressManager();
+
+            List<AddressModel> addressModel = addressManager
+                .ListQueryable()
+                .Where(i => i.MemberId == loggedInMemberId)
+                .Select(i => new AddressModel
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    Description = i.Description
+                })
+                .ToList();
+
+            return View(addressModel);
+        }
+
+        [MemberAuthFilter]
+        public ActionResult CreateNewAddress()
+        {
+            CityManager cityManager = new CityManager();
+
+            ViewBag.Cities = new SelectList(cityManager.GetAll(), "Id", "Name");
+
             return View();
+        }
+
+        [HttpPost]
+        [MemberAuthFilter]
+        public ActionResult CreateNewAddress(AddressViewModel model, bool billType = true)
+        {
+            ModelState.Remove("CountyId");
+
+            if (billType)
+            {
+                ModelState.Remove("CompanyName");
+                ModelState.Remove("TaxNumber");
+                ModelState.Remove("TaxAdministration");
+            }
+            else
+            {
+                ModelState.Remove("CitizenshipNumber");
+            }
+
+            model.MemberId = CurrentSession.Member.Id;
+
+            CityManager cityManager = new CityManager();
+            ViewBag.Cities = new SelectList(cityManager.GetAll(), "Id", "Name");
+
+            if (ModelState.IsValid)
+            {
+                AddressManager addressManager = new AddressManager();
+
+                BusinessLayerResult<Address> layerResult = addressManager.Insert(model);
+
+                if (layerResult.Errors.Count > 0)
+                {
+                    layerResult.Errors.ForEach(x => ModelState.AddModelError("", x.ErrorMessage));
+                    return View(model);
+                }
+
+                return RedirectToAction("MyAddresses");
+            }
+
+            return View(model);
+        }
+
+        [MemberAuthFilter]
+        public ActionResult EditAddress(int? Id)
+        {
+            if (Id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            AddressManager addressManager = new AddressManager();
+            CityManager cityManager = new CityManager();
+            CountyManager countyManager = new CountyManager();
+
+            int loggedInMemberId = CurrentSession.Member.Id;
+
+            BusinessLayerResult<AddressViewModel> layerResult = addressManager.Find(i => i.Id == Id && i.MemberId == loggedInMemberId);
+
+            if (layerResult.Result == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Cities = new SelectList(cityManager.GetAll(), "Id", "Name");
+            ViewBag.Counties = new SelectList(countyManager.GetAll(i => i.CityId == layerResult.Result.CityId), "Id", "Name");
+
+            return View(layerResult.Result);
+        }
+
+        [MemberAuthFilter]
+        [HttpPost]
+        public ActionResult EditAddress(AddressViewModel model, bool billType = true)
+        {
+            if (billType)
+            {
+                ModelState.Remove("CompanyName");
+                ModelState.Remove("TaxNumber");
+                ModelState.Remove("TaxAdministration");
+
+                model.CompanyName = null;
+                model.TaxNumber = null;
+                model.TaxAdministration = null;
+            }
+            else
+            {
+                ModelState.Remove("CitizenshipNumber");
+                model.CitizenshipNumber = null;
+            }
+
+            if (ModelState.IsValid)
+            {
+                AddressManager addressManager = new AddressManager();
+
+                BusinessLayerResult<AddressViewModel> layerResult = addressManager.Update(model);
+
+                if (layerResult.Errors.Count == 0)
+                {
+                    return RedirectToAction("MyAddresses");
+                }
+
+                layerResult.Errors.ForEach(x => ModelState.AddModelError("", x.ErrorMessage));
+            }
+            CityManager cityManager = new CityManager();
+            CountyManager countyManager = new CountyManager();
+
+            ViewBag.Cities = new SelectList(cityManager.GetAll(), "Id", "Name", model.CityId);
+            ViewBag.Counties = new SelectList(countyManager.GetAll(i => i.CityId == model.CityId), "Id", "Name", model.CountyId);
+
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        public JsonResult DeleteAddress(int Id)
+        {
+            int loggedInMemberId = CurrentSession.Member.Id;
+
+            AddressManager addressManager = new AddressManager();
+
+            BusinessLayerResult<Address> layerResult = addressManager.Delete(new Address
+            {
+                Id = Id,
+                MemberId = loggedInMemberId
+            });
+
+            if (layerResult.Errors.Count == 0)
+            {
+                return Json(new { result = true, message = "Adres başarıyla silindi!" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { result = false, message = layerResult.Errors.FirstOrDefault().ErrorMessage }, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Logout()
