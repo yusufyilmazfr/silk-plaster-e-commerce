d9366a5c94c7518da85d9ce6037a9df5d476cbb6
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-27 23:12:26 +0300

    added address model

---
+++ SilkPlaster/SilkPlaster.UI/Models/AddressModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SilkPlaster.UI.Models
+{
+    public class AddressModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
