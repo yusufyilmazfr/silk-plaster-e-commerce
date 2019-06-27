d27ad5cba9a17dce9dabde5430f6a61b853faefc
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-27 23:11:47 +0300

    Cities and counties opened outside as json

---
+++ SilkPlaster/SilkPlaster.UI/Controllers/CountyController.cs
@@ -0,0 +1,30 @@
+﻿using SilkPlaster.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilkPlaster.UI.Controllers
+{
+    public class CountyController : Controller
+    {
+        // GET: County
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCountyWithCities(int cityId)
+        {
+            CountyManager countyManager = new CountyManager();
+
+            var counties = countyManager
+                .ListQueryable()
+                .Where(i => i.CityId == cityId)
+                .ToList();
+
+            return Json(counties, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
