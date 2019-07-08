4a58faabe36eaca14d83317f753931de77f99f34
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-07-08 16:04:01 +0300

    refactoring: foldering, created interface files and some operations made

---
+++ SilkPlaster/SilkPlaster.DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,21 @@
+﻿using SilkPlaster.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.DataAccessLayer.Abstract
+{
+    public interface IProductDal : IRepository<Product>
+    {
+        Product GetProductWithImages(int productId);
+        Product GetProductWithCommentAndImages(int productId);
+        Product GetProductDetails(int productId);
+        List<Product> GetBestSellers(int productCount);
+        List<Product> GetNewProducts(int productCount);
+        List<Product> GetProductsWithCategoriesDesc();
+        List<Product> GetMostCommentedProducts(int productCount);
+        List<Product> GetFeaturedProducts(int productCount);
+    }
+}
---
+++ SilkPlaster/SilkPlaster.DataAccessLayer/Abstract/IWishListDal.cs
@@ -0,0 +1,15 @@
+﻿using SilkPlaster.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.DataAccessLayer.Abstract
+{
+    public interface IWishListDal : IRepository<WishList>
+    {
+        int GetMyWishListCountByMemberId(int memberId);
+        List<WishList> GetMyWishListItemsByMemberId(int memberId);
+    }
+}
