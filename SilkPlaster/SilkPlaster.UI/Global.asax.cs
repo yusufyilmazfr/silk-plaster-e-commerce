ccab279ea4f3ca6e389d855a573611247fa617a1
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-07-08 16:33:12 +0300

    changed default controller

--- SilkPlaster/SilkPlaster.UI/Global.asax.cs
+++ SilkPlaster/SilkPlaster.UI/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SilkPlaster.BusinessLayer.DependencyResolver.Ninject;
+using SilkPlaster.UI.Infrastructure.Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +15,7 @@ namespace SilkPlaster.UI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(new BusinessModule()));
         }
     }
 }
