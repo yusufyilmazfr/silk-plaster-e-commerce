14febe15d51876dae4d2b7553ed10b22523cfbbb
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-07-08 16:29:08 +0300

    refactoring: used Ninject IoC container

---
+++ SilkPlaster/SilkPlaster.UI/Infrastructure/Ninject/NinjectControllerFactory.cs
@@ -0,0 +1,26 @@
+﻿using Ninject;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SilkPlaster.UI.Infrastructure.Ninject
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        public IKernel _kernel { get; set; }
+
+        public NinjectControllerFactory(NinjectModule ninjectModule)
+        {
+            _kernel = new StandardKernel(ninjectModule);
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)_kernel.Get(controllerType);
+        }
+    }
+}
