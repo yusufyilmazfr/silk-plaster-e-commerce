d1ee41a7cb269e3a32cd889eafedeedc6381de1f
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-08-25 14:42:48 +0300

    UnitOfWork design pattern used with entity framework

---
+++ SilkPlaster/SilkPlaster.DataAccessLayer/Abstract/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using SilkPlaster.DataAccessLayer.Abstract.Repository;
+using SilkPlaster.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.DataAccessLayer.Abstract.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges();
+    }
+}
---
+++ SilkPlaster/SilkPlaster.DataAccessLayer/Concrete/EntityFramework/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using SilkPlaster.DataAccessLayer.Abstract.Repository;
+using SilkPlaster.DataAccessLayer.Abstract.UnitOfWork;
+using SilkPlaster.DataAccessLayer.Concrete.EntityFramework.Context;
+using SilkPlaster.DataAccessLayer.Concrete.EntityFramework.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.DataAccessLayer.Concrete.EntityFramework.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DatabaseContext DatabaseContext => (DatabaseContext)_dbContext;
+        private DbContext _dbContext { get; set; }
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            if (dbContext == null)
+                throw new Exception("DbContext can not be null!");
+            _dbContext = dbContext;
+        }
+
+        public int SaveChanges()
+        {
+            using (var scope = _dbContext.Database.BeginTransaction())
+            {
+                int count = 0;
+                try
+                {
+                    count = _dbContext.SaveChanges();
+                    scope.Commit();
+                    return count;
+                }
+                catch
+                {
+                    scope.Rollback();
+                    return count;
+                }
+            }
+        }
+
+        #region Disposable Design Pattern
+
+        private bool disposed = false;
+        public void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
